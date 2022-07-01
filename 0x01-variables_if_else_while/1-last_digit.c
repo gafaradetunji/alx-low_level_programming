e6019fb57bb20089c1adab7ddaf45d2037412ae0
gafaradetunjiqw <gafaradetunji471@gmail.com>
2022-07-01 16:14:42 -0700

    Using Reserved keywords and variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,17 +11,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 5)
+	printf("Last digit of %d is %d", n, n % 10);
+	if ((n%10) > 5)
 	{
-		printf("%d and is greater than 5\n");
+		printf("and is greater than 5\n");
 	}
-	else if (n == 0)
+	else if ((n%10) == 0)
 	{
-		printf("%d and is zero\n");
+		printf("and is 0\n");
 	}
-	else
+	else if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf("%d and is less 6 and not zero\n");
+		printf("and is less 6 and not 0\n");
 	}
 	return (0);
 }
