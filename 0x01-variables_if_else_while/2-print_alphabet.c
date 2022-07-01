da911740769cf2a51b8a0c6e8764a9171d9da824
gafaradetunjiqw <gafaradetunji471@gmail.com>
2022-07-01 15:57:09 -0700

    Using Reserved keywords and variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there
+ *
+* betty style doc for function main goes there
+*/
+int main(void)
+{
+	for(char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+		putchar("\n");
+	}
+	return(0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there
+*betty style doc for function main goes there
+*/
+int main(void)
+{
+	alpha = 'a';
+	ALPHA = 'A';
+	while (alpha = 'z')
+	{
+		putchar(alpha);
+	}
+	while (ALPHA = 'Z')
+	{
+		putchar(ALPHA);
+	}
+	putchar("\n");
+
+	return(0);
+}
