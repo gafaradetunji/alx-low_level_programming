0910f8a3035048aff2ba9d13f8242da157c9a456
gafaradetunjiqw <gafaradetunji471@gmail.com>
2022-07-01 14:33:27 -0700

    if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,11 +9,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-		printf("%d\n", n + "is positive");
-	else if(n == 0)
-		printf("%d\n", n + "is zero");
-	else
-		printf("%d\n", n + "is negative\n");
+
 	return (0);
 }
