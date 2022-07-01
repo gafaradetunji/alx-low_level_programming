0f1e63b41be31b7715c617c3a78b3e1111102a53
gafaradetunjiqw <gafaradetunji471@gmail.com>
2022-07-01 15:14:44 -0700

    Conditional statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,27 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/* more headers goes there
+*
+* betty style doc for function main goes there
+*/
 int main(void)
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-
+	if (n > 5)
+	{
+		printf("%d and is greater than 5\n");
+	}
+	else if (n == 0)
+	{
+		printf("%d and is zero\n");
+	}
+	else
+	{
+		printf("%d and is less 6 and not zero\n");
+	}
 	return (0);
 }
