cb56618d10eb384bc1b0373dce1f8b974821c824
gafaradetunji <gafaradetunji471@gmail.com>
2022-07-02 23:20:41 +0100

    p

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,7 +13,7 @@ int main(void)
         if(letter == 'e')
             letter++;
         else if(letter == 'q')
-            letter++
+            letter++;
         else
             putchar(letter);
             letter++;
