c0084c52cf6f6d4a529b93bdba919483be7d0fe9
gafaradetunji <gafaradetunji471@gmail.com>
2022-07-05 00:01:29 +0100

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,17 @@
  */
 
 int main(void) {
-	int i;
-	double d;
 	char c;
+	int i;
+	long int l;
+	long long int L;
+	float r;
 
-	printf("Size of a double: %lu byte(s)\n",sizeof(d));
+	printf("Size of a char: %lu byte(s)\n",sizeof(c));
+	printf("Size of a int: %lu byte(s)\n",sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n",sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n",sizeof(L));
+	printf("Size of a float: %lu byte(s)\n",sizeof(r));
+
 	return (0);
 }
