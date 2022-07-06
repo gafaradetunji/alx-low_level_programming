9b72bc2b71d54e44f5f09c1c2cd4716fcd63ee85
gafaradetunji <gafaradetunji471@gmail.com>
2022-07-06 01:25:57 +0100

    correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,6 +4,19 @@
 * Main - prints out letters in lower case
 */
 
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
+
 void print_alphabet_x10(void)
 {
 	int i;
