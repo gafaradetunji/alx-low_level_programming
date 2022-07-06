6727071dbf3844e81990d08943e37e619777253c
gafaradetunji <gafaradetunji471@gmail.com>
2022-07-06 22:31:10 +0100

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 /** more headers goes there 
 *
 * 
 */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if(n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+	int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        if(i == 0){
+            printf('%d is zero\n', i);
+        }
+
+        return (0);
 }
