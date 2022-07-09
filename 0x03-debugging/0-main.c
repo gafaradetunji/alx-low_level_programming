2ab80f55a59f312fe0394b4bdf8b0ca8120e4881
gafaradetunji <gafaradetunji471@gmail.com>
2022-07-09 01:36:06 +0100

    pop

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,12 +7,8 @@ int main(void)
 {
 	int i;
 
-        i = 98;
+        i = 0;
         positive_or_negative(i);
-
-        if(i == 0){
-            printf('%d is zero\n', i);
-        }
 
         return (0);
 }
